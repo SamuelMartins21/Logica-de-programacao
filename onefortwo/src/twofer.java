ee4945c061fc479ab04d2838cf7287ca9356108f
Samuel Martins <samuelsilvaa431@gmail.com>
2023-08-17 10:41:59 -0300

    Treinando if

---
+++ onefortwo/src/twofer.java
@@ -0,0 +1,9 @@
+public class twofer {
+    
+    public String twoffer(String name){
+        if(name == null){
+            return "One for you, one for me.";
+        } 
+        return "One for " + name + ",one for me.";
+    }
+}
