f66c894be9152f43212395c820f3c7e59a76c8d9
Samuel Martins <samuelsilvaa431@gmail.com>
2023-10-12 19:11:12 -0300

    Lógica entre String e Arrays

---
+++ ArraysStrings/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        String[] words = {"Samuel", "Raissa",};
+        Proverb testProverb = new Proverb(words);
+        testProverb.recite();
+    }
+}
---
+++ ArraysStrings/src/Proverb.java
@@ -0,0 +1,13 @@
+public class Proverb {
+    private String[] words;
+    private String proverbio1;
+    private String proverbio2;
+    
+    public Proverb(String[] words) {
+        this.words = words;
+    }
+
+    public String recite() {
+        return proverbio1;
+    }
+}
