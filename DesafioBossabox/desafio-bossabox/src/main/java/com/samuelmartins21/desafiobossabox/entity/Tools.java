357ad8540746b22507cb894a20779c68dfd7a1b3
Samuel Martins <samuelsilvaa431@gmail.com>
2024-03-09 09:14:47 -0300

    Criando entidade tools

---
+++ DesafioBossabox/desafio-bossabox/src/main/java/com/samuelmartins21/desafiobossabox/entity/Tools.java
@@ -0,0 +1,83 @@
+package com.samuelmartins21.desafiobossabox.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Tools implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private String description;
+
+    private List<String> tags;
+
+    public Tools(Long id, String title, String link, String description, List<String> tags) {
+        this.id = id;
+        this.title = title;
+        this.link = link;
+        this.description = description;
+        this.tags = tags;
+    }
+
+    public Tools() {
+    };
+
+    public static long getSerialversionuid() {
+        return serialVersionUID;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+}
